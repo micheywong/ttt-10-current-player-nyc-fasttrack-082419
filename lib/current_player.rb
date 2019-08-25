7857f958d47170d6564a02eb3cc84d296711a197
Michelle Wong <mwong@conductor.com>
2019-08-25 16:17:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,14 @@
-# objective is to count how many turns have been played
+#objective is to count how many turns have been played
 #strategy1 
-def turn_count(board)
-  arr = {}
-    board.each do |position|
-      if position == "X" || position == "O"
-        arr.push(1)
-      end
-    end
-  return arr.length 
-end
+#def turn_count(board)
+#  arr = {}
+#    board.each do |position|
+#      if position == "X" || position == "O"
+#        arr.push(1)
+#      end
+#    end
+#  return arr.length 
+#end
 
 #strategy2
 def turn_count(board)
