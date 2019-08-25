c4ced5bea6bc1b6dbe296bee30bb1d340cf465eb
Michelle Wong <mwong@conductor.com>
2019-08-25 16:04:50 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,5 +2,8 @@ def turn_count(board)
     arr = {}
     board.each do |position|
       if position == "X" || position == "O"
-        arr << 
+        arr.push(1)
+      end
+      return arr.length 
+end
       
